a3a1b31005b9527c3efd029495c2d20eabc94b5a
Steffen Krause <steffen.krause@soabridge.com>
2018-07-06 23:14:38 -0400

    Added documentation to the main file

--- sandbox3/main.go
+++ sandbox3/main.go
@@ -8,24 +8,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Create a new logger to be used in the sandbox3 application
+//                     +---------------------------------- Writer for the log output
+//                     |            +--------------------- Prefix to use fo this logger's messages
+//                     |            |             +------- Format for additional log info (e.g. date, time, file)
+//                     |            |             |
 var logger = log.New(os.Stdout, "greeter: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
 
-func ServerAPI() {
-	handler := &eventHandler{}
-	practicerouter := mux.NewRouter().PathPrefix("/greeter").Subrouter()
-	practicerouter.Methods("GET").Path("/hello").HandlerFunc(handler.handleGenericHello)
-	practicerouter.Methods("GET").Path("/hello/{name}").HandlerFunc(handler.handleSpecificHello)
-	http.ListenAndServe(":12345", practicerouter)
-}
-
 func main() {
-	log.SetPrefix("startup: ")
-	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile)
+	// Configure default/static logger
+	log.SetPrefix("startup: ")                                               // Set prefix to use
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile) // Set format of additional info
+	// Logging that that the program execution has started
 	log.Println("--== Let's get started ==--")
+	// Initialize the Gorilla Web Toolkit and configure the handlers
 	handler := &eventHandler{}
 	practicerouter := mux.NewRouter().PathPrefix("/greeter").Subrouter()
 	practicerouter.Methods("GET").Path("/hello").HandlerFunc(handler.handleGenericHello)
 	practicerouter.Methods("GET").Path("/hello/{name}").HandlerFunc(handler.handleSpecificHello)
+	// Start a HTTP listener on port 12345 and configure the Gorilla Web Toolkit router
+	// as the main router for incoming traffic.
 	log.Fatal(http.ListenAndServe(":12345", practicerouter))
 
 	// sig := make(chan os.Signal, 1)
